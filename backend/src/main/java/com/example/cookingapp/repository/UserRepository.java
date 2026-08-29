39addfd1e7e11a8c8480b099604e1619da208030
akihide-hatano <a.h.19841202@gmail.com>
2026-08-29 17:03:37 +0900

    UserRepositoryにてOptionalを返すように修正

--- backend/src/main/java/com/example/cookingapp/repository/UserRepository.java
+++ backend/src/main/java/com/example/cookingapp/repository/UserRepository.java
@@ -2,4 +2,6 @@ package com.example.cookingapp.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
