1d3c20a445c9cca805a1dfa39cd6334438154870
akihide-hatano <a.h.19841202@gmail.com>
2026-08-29 17:10:49 +0900

    Add repositories

--- backend/src/main/java/com/example/cookingapp/repository/UserRepository.java
+++ backend/src/main/java/com/example/cookingapp/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.cookingapp.entity;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+  Optional<User> findByEmail(String email);
 }
