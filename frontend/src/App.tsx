ec2240ce722c8f1a56e2bd83f7fae634323be061
akihide-hatano <a.h.19841202@gmail.com>
2026-09-17 07:42:51 +0900

    userのレシピ新規作成画面のルーティングと最小の画面作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./features/auth/LoginPage";
 import { RegisterPage } from "./features/auth/RegisterPage";
 import { UserPage } from "./features/user/UserPage";
-
+import { RecipeCreatePage } from "./features/recipe/RecipeCreatePage";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/user" element={<UserPage />} />
+        <Route path="/recipes/new" element={<RecipeCreatePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/features/recipe/RecipeCreatePage.tsx
@@ -0,0 +1,8 @@
+export const RecipeCreatePage = () => {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold">Recipe Create Page</h1>
+      <p className="mt-4 text-gray-600">ここでレシピを投稿できます。</p>
+    </div>
+  );
+};
